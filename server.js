abe45c0521f80c244a408c55d1ec0c953854b02c
chai-dech <chaidechks@gmail.com>
2021-05-19 23:50:31 +0530

    feat/added server.js file

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
-console.log("test");
+const express = require('express');
+const dotenv = require('dotenv');
+const app = express();
+
+dotenv.config({path: 'config.env'})
+const Port = process.env.Port;
+
+app.listen(Port, ()=>{
+    console.log(`Server is up and is running on: http://localhost:${Port}`);
+})
+
+app.get('/', (req,res)=>{
+res.send("Customer service application")
+})
+
