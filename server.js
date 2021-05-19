f4eb670af6b03f12a8d63e6279be419d07b7ff97
chai-dech <chaidechks@gmail.com>
2021-05-20 00:08:19 +0530

    added morgan,bodyParser and template engine

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
 const app = express();
 
 dotenv.config({path: 'config.env'})
@@ -8,6 +9,13 @@ const Port = process.env.Port;
 app.listen(Port, ()=>{
     console.log(`Server is up and is running on: http://localhost:${Port}`);
 })
+
+//logs the requests
+app.use(morgan('tiny'));
+//use the bodyparser 
+app.use(express.urlencoded({ extended: true }));
+//set view engine
+app.set('view engine', 'ejs');
 
 app.get('/', (req,res)=>{
 res.send("Customer service application")
