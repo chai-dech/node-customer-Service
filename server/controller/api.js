3007e39458dddc0f66e16180f06935108a6e1681
chai-dech <chaidechks@gmail.com>
2021-05-22 15:38:51 +0530

    update and delete user

--- server/controller/api.js
+++ server/controller/api.js
@@ -60,11 +60,46 @@ exports.find = (req, res)=>{
     }}
 
 //update user
-exports.update= (req, res) =>{
 
+exports.update = (req, res)=>{
+    if(!req.body){
+        return res
+            .status(400)
+            .send({ message : "Data to update can not be empty"})
+    }
+
+    const id = req.params.id;
+    userDb.findByIdAndUpdate(id, req.body)
+        .then(data => {
+            if(!data){
+                res.status(404).send({ message : `Cannot Update user with ${id}. Maybe user not found!`})
+            }else{
+                res.send(data)
+            }
+        })
+        .catch(err =>{
+            res.status(500).send({ message : "Error Update user information"})
+        })
 }
 
-//delete user
-exports.delete= (req, res) =>{
 
+//delete user
+exports.delete = (req, res)=>{
+    const id = req.params.id;
+
+    userDb.findByIdAndDelete(id)
+        .then(data => {
+            if(!data){
+                res.status(404).send({ message : `Cannot Delete with id ${id}. Maybe id is wrong`})
+            }else{
+                res.send({
+                    message : "User was deleted successfully!"
+                })
+            }
+        })
+        .catch(err =>{
+            res.status(500).send({
+                message: "Could not delete User with id=" + id
+            });
+        });
 }
