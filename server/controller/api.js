2a139179ea60b13f9c0751a76c2bfe4f2d172e5f
chai-dech <chaidechks@gmail.com>
2021-05-22 15:21:38 +0530

    find user api

--- server/controller/api.js
+++ server/controller/api.js
@@ -32,9 +32,33 @@ exports.create = (req,res)=>{
 
 
 //find user
-exports.find= (req, res) =>{
+exports.find = (req, res)=>{
 
-}
+    if(req.query.id){
+        const id = req.query.id;
+
+        userDb.findById(id)
+            .then(data =>{
+                if(!data){
+                    res.status(404).send({ message : "Not found user with id " , id})
+                }else{
+                    res.send(data)
+                }
+            })
+            .catch(err =>{
+                res.status(500).send({ message: "Error retrieving user with id " , id})
+            })
+
+    }else{
+        userDb.find()
+            .then(user => {
+                res.send(user)
+            })
+            .catch(err => {
+                res.status(500).send({ message : err.message || "Error Occurred while retriving user information" })
+            })
+    }}
+
 //update user
 exports.update= (req, res) =>{
 
